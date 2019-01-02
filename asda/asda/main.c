27ee85b19956f49e5f30e15e92ca33f8b4eb2529
peterhjz <603460027@qq.com>
2019-01-02 23:53:12 +0800

    First Commit

---
+++ asda/asda/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  asda
+//
+//  Created by Peter 何 on 2019/1/2.
+//  Copyright © 2019 Perter he. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    printf("Hello, World!\n");
+    printf("i love you!\n");
+    return 0;
+}
